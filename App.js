63d1c46f4741b1119986c266c3f05eb3ca186ae7
jantsigit <83158197+jantsigit@users.noreply.github.com>
2021-11-01 18:33:39 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import{ StyleSheet, Text, View, Button, Alert, TextInput, Image} from'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ensimmäinen kokeilu on tässä!</Text>
+      <Text>Hello world!</Text>
+      <Button onPress={buttonPressed}title="Paina tästä" />
+      <Image style={{  width:250, height:100 }}source={require('./img/logo.png')}  />
       <StatusBar style="auto" />
     </View>
   );
 }
+const buttonPressed= () =>{Alert.alert('Painettu on');}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
